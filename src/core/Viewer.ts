e943cbae4f6d55424f4b9307bbb3b24bc1dddfe7
巫龙斌 <wulongbin@wunsun.com>
2023-04-11 16:11:48 +0800

    更新

--- src/core/Viewer.ts
+++ src/core/Viewer.ts
@@ -56,34 +56,47 @@ export default class Viewer{
       (this.viewer.cesiumWidget.creditContainer as any).style.display = "none";
       this.scene = this.viewer.scene;
 
-      this.viewer.imageryLayers.removeAll();
+      this.viewer.imageryLayers.remove(this.viewer.imageryLayers.get(0))
       this.viewer.imageryLayers.addImageryProvider(
         new Cesium.WebMapTileServiceImageryProvider(
           {
-            url: 'http://t0.tianditu.gov.cn/img_w/wmts?tk=589d716f51345fac17b7942ab3eb62e5',
+            url: 'http://{s}.tianditu.gov.cn/img_w/wmts?tk=589d716f51345fac17b7942ab3eb62e5',
             layer:'img',
             style:'default',
             tileMatrixSetID:'w',
+            subdomains: ["t0", "t1", "t2", "t3", "t4", "t5", "t6", "t7"],
             format:'tiles',
             maximumLevel: 18}
+        //   {
+        //     //影像底图
+        //     url:
+        //         'http://t0.tianditu.gov.cn/img_c/wmts?SERVICE=WMTS&REQUEST=GetTile&VERSION=1.0.0&LAYER=img&STYLE=default&TILEMATRIXSET=w&FORMAT=tiles&TILEMATRIX={z}&TILEROW={x}&TILECOL={y}&tk='+'589d716f51345fac17b7942ab3eb62e5',
+        //     // subdomains: subdomains,//URL模板中用于{s}占位符的子域。如果该参数是单个字符串，则字符串中的每个字符都是一个子域。如果它是一个数组，数组中的每个元素都是一个子域
+        //     layer: "tdtAnnoLayer",
+        //     style: 'default',
+        //     format: "image/jpeg",
+        //     // subdomains: ["t0", "t1", "t2", "t3", "t4", "t5", "t6", "t7"],
+        //     tileMatrixSetID: 'w', //使用谷歌的瓦片切片方式
+        //     show: true
+        // } as any
         )
     );
-    return;
+    // return;
 
-      const instance = useCesium(this.viewer);
+      // const instance = useCesium(this.viewer);
     
-      //添加星图图层
-      instance.addLayer({
-          name: 'yingxiang',
-          index: 0,
-          baseUrl: "https://tiles1.geovisearth.com/base/v1/img/{z}/{x}/{y}",
-          tmsIds: "w",
-          visible:true,
-          format:'webp',
-          token: "bf78bb0fe9bf35f87c6d4c9b4ac4a3a69fbccb87f90712777936e9a662c9718e",
-          minimumLevel: 0,
-          maximumLevel: 18,
-      } as any);
+      // //添加星图图层
+      // instance.addLayer({
+      //     name: 'yingxiang',
+      //     index: 0,
+      //     baseUrl: "https://tiles1.geovisearth.com/base/v1/img/{z}/{x}/{y}",
+      //     tmsIds: "w",
+      //     visible:true,
+      //     format:'webp',
+      //     token: "bf78bb0fe9bf35f87c6d4c9b4ac4a3a69fbccb87f90712777936e9a662c9718e",
+      //     minimumLevel: 0,
+      //     maximumLevel: 18,
+      // } as any);
 
     }
 
